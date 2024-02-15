407b8f7b3c3aa2a4f0c4353d65dbad8e72942638
manishtmtmt <manisht.mt.mt@gmail.com>
2024-02-15 16:26:48 +0530

    web complier project initialized

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
+
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
+import { Compiler } from "./pages/Compiler";
+import { NotFound } from "./pages/NotFound";
+import { ThemeProvider } from "./components/theme-provider";
+
+function App() {
+  return (
+    <>
+      <Toaster />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/compiler" element={<Compiler />} />
+          <Route path="/compiler/:urlId" element={<Compiler />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Compiler.tsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { toast } from "sonner";
+
+import { CodeEditor } from "@/components/CodeEditor";
+import { HelperHeader } from "@/components/HelperHeader";
+import RenderCode from "@/components/RenderCode";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { updateFullCode } from "@/redux/slices/compilerSlice";
+import { handleError } from "@/utils/handleError";
+import { useEffect } from "react";
+
+export const Compiler = () => {
+  const { urlId } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadCode = async () => {
+      try {
+        const response = await axios.post(
+          "https://web-compiler-server.vercel.app/compiler/load",
+          {
+            urlId,
+          }
+        );
+        dispatch(updateFullCode(response.data.fullCode));
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          if (error?.response?.status === 500) {
+            toast("Invalid URL, Default Code Loaded");
+          }
+        }
+        handleError(error);
+      }
+    };
+
+    if (urlId) loadCode();
+  }, [dispatch, urlId]);
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel
+        className="h-[calc(100dvh-60px)] min-w-[350px]"
+        defaultSize={50}
+      >
+        <HelperHeader />
+        <CodeEditor />
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel
+        className="h-[calc(100dvh-60px)] min-w-[350px]"
+        defaultSize={50}
+      >
+        <RenderCode />
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
